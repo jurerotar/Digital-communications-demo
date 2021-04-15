55dcdc0d156d0d568e5d2526a7710b6dd5d8d211
Jure Rotar <jrotar0@gmail.com>
2021-04-15 17:14:21 +0200

    Canvases are now scrollable on mobile devices

---
+++ src/components/global/CanvasContainer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class = "flex overflow-x-scroll lg:overflow-x-hidden">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CanvasContainer"
+}
+</script>
