a1c41541fde513e6d729567571b357f01b32364f
Jure Rotar <jrotar0@gmail.com>
2021-04-15 17:09:16 +0200

    Added Montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+
 module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: 'class', // or 'media' or 'class'
     theme: {
-        extend: {},
+        container: {
+            screens: {
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+            }
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+            },
+            maxWidth: {
+                'sidebar-mobile': '16rem',
+                'sidebar': '18rem',
+                'top-bar': 'calc(100% - 18rem)',
+            },
+        },
     },
     variants: {
         extend: {},
