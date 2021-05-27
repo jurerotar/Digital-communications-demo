99f39c6162b43f5d8e402ab360443a59d821e2c5
Jure Rotar <jrotar0@gmail.com>
2021-05-27 14:32:52 +0200

    Added arrows and proper value scaling

--- src/components/canvas/IndividualHarmonics.vue
+++ src/components/canvas/IndividualHarmonics.vue
@@ -26,23 +26,18 @@ export default {
             texts: [
                 {
                     text: '1',
-                    x: 130,
-                    y: 15,
+                    x: 12,
+                    y: -105,
                 },
                 {
                     text: '1',
-                    x: 230,
-                    y: 140,
-                },
-                {
-                    text: 'y = sin(t)',
-                    x: 70,
+                    x: 110,
                     y: 15,
                 },
                 {
-                    text: 't',
-                    x: 680,
-                    y: 140,
+                    text: 'y = sin(t)',
+                    x: -23,
+                    y: -140,
                 },
             ]
         }
@@ -57,23 +52,37 @@ export default {
             this.$c.setup(p5);
             p5.draw = () => {
 
-                // Add context to allow custom canvas transformations
-                this.context = document.querySelector(`#${this.canvasId} canvas`).getContext('2d');
-
-                // Recolor canvas on each draw to remove previous lines
+                p5.stroke(0);
+                //this.max = this.$c.max(this.data.map(el => Math.abs(el)));
                 p5.background(255);
+                const canvasDimensions = this.$c.dimensions;
+                const canvasPadding = 50;
+                p5.strokeWeight(2);
+                p5.line(canvasDimensions.x / 2-this.offset.x, canvasPadding-30, canvasDimensions.x / 2-this.offset.x, canvasDimensions.y - canvasPadding+20);
+                p5.strokeWeight(1);
+                //adds arrow on x-axis
+                p5.strokeWeight(2);
+                this.drawArrow(p5, p5.createVector(0, canvasDimensions.y/2), p5.createVector(canvasDimensions.x-30, canvasDimensions.y / 2), 'black', 7, 0);
+                //adds arrow on y-axis
+                p5.fill(1);
+                p5.triangle(175, 20, 171, 27, 179, 27);
+                //adds texts on axis
+                p5.strokeWeight(1);
+                p5.textSize(12);
+                p5.textFont('Montserrat');
+                p5.text('t', canvasDimensions.x - 15, canvasDimensions.y / 2 + 3);
 
-                // Draw axis and move center to defined offset coordinates
-                this.$c.drawAxis(p5, this.offset);
-                this.texts.forEach(el => p5.text(el.text, el.x, el.y));
+                this.context = document.querySelector(`#${this.canvasId} canvas`).getContext('2d');
 
                 // Dashed lines to show values
                 this.$c.drawDashed(this.context, () => {
-                    p5.line(0, 22, 700, 22);
-                    p5.line(0, 228, 700, 228);
+                    p5.line(0, 25+canvasPadding/2, 700, 25+canvasPadding/2);
+                    p5.line(0, 225+canvasPadding/2, 700, 225+canvasPadding/2);
                 });
 
-                p5.translate(this.offset.x, this.offset.y);
+                p5.translate(this.offset.x, this.offset.y+canvasPadding/2);
+
+                this.texts.forEach(el => p5.text(el.text, el.x, el.y));
 
                 p5.noFill();
                 p5.strokeWeight(3);
@@ -102,12 +111,12 @@ export default {
                     // Draw sine wave
                     p5.beginShape();
                     this.waveValues[i].forEach((y, x) => {
-                        p5.vertex(x + 200, y);
+                        p5.vertex(x + 150, y);
                     });
                     p5.endShape();
                     this.$c.drawDashed(this.context, () => {
                         // Draw a line from x, y to start of sine wave which is at position (200, first element of array)
-                        p5.line(x, y, 200, this.waveValues[i][0]);
+                        p5.line(x, y, 150, this.waveValues[i][0]);
                     });
 
                     this.$c.drawArrow(p5, p5.createVector(0, 0), p5.createVector(x * 0.99, y * 0.99), color, 7);
@@ -124,6 +133,19 @@ export default {
     unmounted() {
         // Remove canvas, otherwise P5 object stays in memory
         this.p5.removeCanvas();
+    },
+    methods: {
+        drawArrow(p5, vectorStart, vectorEnd, color, size = 7, rotate = 0) {
+            const arrowSize = size;
+            p5.push();
+            p5.stroke(color);
+            p5.fill(color);
+            p5.line(vectorStart.x, vectorStart.y, vectorEnd.x, vectorEnd.y);
+            p5.rotate(rotate);
+            p5.translate(vectorEnd.x, vectorEnd.y);
+            p5.triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+            p5.pop();
+        },
     },
     props: {
         components: {
