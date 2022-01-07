22f27478fdbb4abd2dfab72266f62bfa0d7bf4cb
Aljaz Zadravec <aljaz.zadravec99@gmail.com>
2022-01-07 09:25:31 +0100

    added correlation.vue

---
+++ src/components/theory/TheoryCorrelation.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p class="transition-colors duration-300 dark:text-white">
+            {{formula}}
+        </p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "TheoryCorrelation",
+        data(){
+            return{
+                formula: '$R_{xy}=\\overline{x(t)y(t)}={1\\over T}\\int_Tx(t)y(t)dt$'
+            }
+        },
+        methods: {
+             reRender() {
+            if(window.MathJax) {
+                console.log('rendering mathjax');
+                 window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub], () => console.log('done'));
+                }
+            }
+        },
+        mounted() {
+            this.reRender();
+        },
+        watch: {
+            latex: function() {
+                console.log('data changed')
+                this.reRender();
+            }
+        }
+    }
+</script>
---
+++ src/pages/Correlation.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="lg:ml-72">
+        <main class="p-4 container lg:mx-auto transition-colors duration-300 dark:bg-gray-800">
+            <h1 class="text-3xl font-medium mb-4 transition-colors duration-300 dark:text-white">Korelacijska Funkcija</h1>
+            <collapsible>
+                <theory-correlation></theory-correlation>
+            </collapsible>
+        </main>
+    </div>
+</template>
+
+<script>
+    import Collapsible from "@/components/global/Collapsible"
+    import TheoryCorrelation from '../components/theory/TheoryCorrelation'
+
+    export default {
+        name: "Correlation",
+        components: {Collapsible, TheoryCorrelation},
+
+    }
+</script>
