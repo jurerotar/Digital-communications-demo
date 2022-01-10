c87eef5a6b24ee9f2241bfdd90477306440a33b9
Aljaz Zadravec <aljaz.zadravec99@gmail.com>
2022-01-10 18:13:43 +0100

    Added frequency selector and tau slider

--- src/pages/Correlation.vue
+++ src/pages/Correlation.vue
@@ -5,30 +5,37 @@
             <collapsible>
                 <theory-correlation></theory-correlation>
             </collapsible>
+
             <h2 class="font-semibold text-xl mb-2 transition-colors duration-300 dark:text-white">Korelacija med:</h2>
             <button-container>
                 <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signal.label===signalType}" v-for="signal in signals" :key=signal.label @click="changeType(signal.label)">
                     {{signal.label}}
                 </styled-button>
             </button-container>
-            <h2 class="font-semibold text-xl mb-2 transition-colors duration-300 dark:text-white" v-show="signalType==='Impulzni'">Izbira prvega signala:</h2>
-            <button-container v-if="signalType==='Impulzni'">
-                <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signals[1].choosedFirstSignal===signal.key}" v-for="signal in signals[1].signal" :key=signal.key @click="changeSignal(signal.key, 'impulzni', 1)">
-                    {{signal.label}}
-                </styled-button>
-            </button-container>
-            <h2 class="font-semibold text-xl mb-2 transition-colors duration-300 dark:text-white">Izbira drugega signala:</h2>
-            <button-container v-if="signalType==='Harmonični'">
-                <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signals[0].choosedSignal===signal.key}" v-for="signal in signals[0].signal" :key=signal.key @click="changeSignal(signal.key, 'harmonični')">
-                    {{signal.label}}
-                </styled-button>
-            </button-container>
-            <button-container v-if="signalType==='Impulzni'">
-                <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signals[1].choosedSecondSignal===signal.key}" v-for="signal in signals[1].signal" :key=signal.key @click="changeSignal(signal.key, 'impulzni', 2)">
-                    {{signal.label}}
-                </styled-button>
-            </button-container>
+
             <template v-if="signalType==='Harmonični'">
+                <h2 class="font-semibold text-xl mb-2 transition-colors duration-300 dark:text-white">Izbira drugega signala:</h2>
+                <button-container>
+                    <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signals[0].choosedSignal===signal.key}" v-for="signal in signals[0].signal" :key=signal.key @click="changeSignal(signal.key, 'harmonični')">
+                        {{signal.label}}
+                    </styled-button>
+                </button-container>
+                <h2 class="font-semibold text-xl mb-2 transition-colors duration-300 dark:text-white">Izbira frekvence drugega signala:</h2>
+                <button-container>
+                    <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signals[0].frequency===freq}" v-for="freq in signals[0].freqRange" :key=freq @click="changeFrequency(freq)">
+                        {{freq}}
+                    </styled-button>
+                </button-container>
+                <!-- Slider -->
+                <div class="inline-flex flex-col mb-2">
+                <label :for="'tau'" class="text-xl transition-colors duration-300 dark:text-white">Časovni zamik drugega signala <span class="font-semibold text-2xl">τ</span>: <span
+                    class="font-medium"> {{ this.tau }}</span></label>
+                <input type="range" :id="'tau'"
+                       v-model.number="tau"
+                       @change="updateTau(tau)"
+                       min="-1" max="1" step="0.1"
+                >
+                </div>
                 <full-signal
                     :canvas_id="'first-signal'"
                     :data="canvasInputFirstH"
@@ -38,7 +45,7 @@
                     :type = type.firstSignal
                 >
                 </full-signal>
-                <!--<full-signal
+                <full-signal
                     :canvas_id="'second-signal'"
                     :data="canvasInputSecondH"
                     :title="'Drugi Signal'"
@@ -55,9 +62,22 @@
                     :horizontal_pool="horizontal_pool"
                     :type = type.correlation
                 >
-                </full-signal>-->
+                </full-signal>
             </template>
+
             <template v-if="signalType==='Impulzni'">
+                <h2 class="font-semibold text-xl mb-2 transition-colors duration-300 dark:text-white">Izbira prvega signala:</h2>
+                <button-container>
+                    <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signals[1].choosedFirstSignal===signal.key}" v-for="signal in signals[1].signal" :key=signal.key @click="changeSignal(signal.key, 'impulzni', 1)">
+                        {{signal.label}}
+                    </styled-button>
+                </button-container>
+                <h2 class="font-semibold text-xl mb-2 transition-colors duration-300 dark:text-white">Izbira drugega signala:</h2>
+                <button-container>
+                    <styled-button :class="{'dark:bg-blue-500 bg-blue-500': signals[1].choosedSecondSignal===signal.key}" v-for="signal in signals[1].signal" :key=signal.key @click="changeSignal(signal.key, 'impulzni', 2)">
+                        {{signal.label}}
+                    </styled-button>
+                </button-container>
                 <!--<full-signal
                     :canvas_id="'first-signal'"
                     :data="canvasInputI"
@@ -104,39 +124,54 @@
             return{
                 signalType: "Harmonični",
                 signalSize: 2**11,
+                xAxisArray: null,
                 firstSignalArray: null, 
                 secondSignalArray: null, 
                 correlationSignalArray: null,
                 frequency: null,
                 horizontal_pool: [-1.5, -1.25, -1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1, 1.25, 1.5],
                 typeLabel: 0, //0 for "Harmonični", 1 for "Impulzni"
+                tau: 0,
                 type: {
                     firstSignal: "first",
                     secondSignal: "second",
                     correlation: "correlation"
+                },
+                correlation: {
+                    drawingValues: () => this.multiplyArrays(this.firstSignalArray, this.secondSignalArray, this.xAxisArray.length)           
                 },
                 signals: [
                     {
                         label: "Harmonični",
                         choosedSignal: "sin",
                         horizontal_pool: [-1.5, -1.25, -1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1, 1.25, 1.5],
+                        freqRange: [1, 2, 3, 4, 5],
+                        frequency: 1,
                         baseHarmonicSignal: {
                                 key: "baseCos",
-                                drawingValues: () => this.firstSignalArray.map(t => (Math.cos(Math.PI * t * 1 * 0.01 + 0.75))),    //0.75 je pi/2 
+                                drawingValues: () => {
+                                    this.firstSignalArray = this.xAxisArray.map(t => (Math.cos(Math.PI * t * 1 * 0.01 + 0.75)))   //0.75 je pi/2 
+                                    return this.firstSignalArray
+                                },
                         },
                         signal: [
                             {
                                 key: "sin",
-                                label: "Sin(2πf*t)",
-                                drawingValues: () => 0
+                                label: "Sin(2πft)",
+                                drawingValues: () => {
+                                    this.secondSignalArray = this.xAxisArray.map(t => (Math.sin(Math.PI * (t + 200*this.tau/this.signals[0].frequency) * this.signals[0].frequency * 0.01 + (0.75 - (this.signals[0].frequency-1)*2.4))))   //Začnemo z 0.75 in za vsako frekvenco se odšteje 2.4
+                                    return this.secondSignalArray
+                                },
                             },
                             {
                                 key: "cos",
-                                label: "Cos(2πf*t)",
-                                drawingValues: () => 0
-                            }
+                                label: "Cos(2πft)",
+                               drawingValues: () => {
+                                    this.secondSignalArray = this.xAxisArray.map(t => (Math.cos(Math.PI * (t + 200*this.tau/this.signals[0].frequency) * this.signals[0].frequency * 0.01 + (0.75 - (this.signals[0].frequency-1)*2.4))))
+                                    return this.secondSignalArray
+                                },
+                            },
                         ],
-                        freqRange: null
                     },
                     {
                         label: "Impulzni",
@@ -200,9 +235,23 @@
             cutArray(array) {
                 const size = 600;
                 return array.filter((el, index) => (index <=  size));
-            }
+            },
+            multiplyArrays(array1, array2, arrayLength){
+                let multipliedArray = []
+                for(let i = 0; i < arrayLength; i++){
+                    multipliedArray[i] = -1*array1[i]*array2[i]
+                }
+                return multipliedArray
+            },
+            changeFrequency(frequency){
+                this.signals[0].frequency = frequency
+            },
+            updateTau(value) {
+            this.tau = value;
+        },
         },
         created(){
+            this.xAxisArray = [...[...Array(this.signalSize/2).keys()].map(el => el * -1 - 1).reverse(), ...Array(this.signalSize/2).keys()]
             this.firstSignalArray = [...[...Array(this.signalSize/2).keys()].map(el => el * -1 - 1).reverse(), ...Array(this.signalSize/2).keys()]
             this.secondSignalArray = [...[...Array(this.signalSize/2).keys()].map(el => el * -1 - 1).reverse(), ...Array(this.signalSize/2).keys()]
             this.correlationSignalArray = [...[...Array(this.signalSize/2).keys()].map(el => el * -1 - 1).reverse(), ...Array(this.signalSize/2).keys()]
@@ -210,18 +259,12 @@
         computed: {
             canvasInputFirstH() {
                 return this.cutArray(this.signals[0].baseHarmonicSignal.drawingValues())
-                    //this.cutArray(this.signals[0].signal.find(el => el.key === this.choosedSignal).drawingValues()),
-                    //this.cutArray(this.signals[0].co.drawingValues())
             },
             canvasInputSecondH() {
-                return this.cutArray(this.signals[this.typeLabel].baseHarmonicSignal.drawingValues())
-                    //this.cutArray(this.signals[0].signal.find(el => el.key === this.choosedSignal).drawingValues()),
-                    //this.cutArray(this.signals[0].co.drawingValues())
+                return this.cutArray(this.signals[0].signal.find(el => el.key === this.signals[0].choosedSignal).drawingValues())
             },
             canvasInputCorrelationH() {
-                return this.cutArray(this.signals[0].baseHarmonicSignal.drawingValues())
-                    //this.cutArray(this.signals[0].signal.find(el => el.key === this.choosedSignal).drawingValues()),
-                    //this.cutArray(this.signals[0].co.drawingValues())
+                return this.cutArray(this.correlation.drawingValues())
             },
             signal(){
                 return this.findSignal()
