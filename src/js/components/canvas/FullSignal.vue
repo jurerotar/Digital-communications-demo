1668a0f0dae6c210f6c7b83a5fe1584efb3f3014
aljazzadravec <aljaz.zadravec99@gmail.com>
2022-02-09 19:49:20 +0100

    y axis labeling added

--- src/js/components/canvas/FullSignal.vue
+++ src/js/components/canvas/FullSignal.vue
@@ -118,7 +118,7 @@ export default {
             }
           }
           //adds texts on axis
-          p5.text('x(t)', canvasDimensions.x / 2 - 5 - canvasPadding, 30);
+          p5.text(this.yAxisLabel, canvasDimensions.x / 2 - 5 - canvasPadding, 30);
           p5.text('t', canvasDimensions.x - 25 - canvasPadding, canvasDimensions.y / 2 + 15);
           //adds arrow on x-axis
           p5.strokeWeight(2);
